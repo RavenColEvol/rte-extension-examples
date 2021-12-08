7c04d59b3a378f4f054d85ab7acee9437a77a0f0
Ravi Lamkoti <ravilamkoti@gmail.com>
2021-12-08 11:11:22 +0530

    add: highlight

---
+++ highlight/src/plugin.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Icon } from '@contentstack/venus-components'
+import ContentstackSDK from "@contentstack/app-sdk";
+
+
+export default ContentstackSDK.init().then(async (sdk) => {
+    const extensionObj = await sdk["Extension"];
+    const RTE = await extensionObj["RTEPlugin"];
+    if(!RTE) return ;
+
+    const Highlight = RTE('highlight', () => ({
+        title: 'Highlight',
+        iconName: <Icon style={{padding: '0 6px'}} icon="Edit" size="original" />,
+        Component: (props) => {
+            return <span style={{background: 'rgba(251, 243, 219, 1)'}}>{props.children}</span>
+        },
+        display: ['toolbar'],
+        elementType: ['text']
+    }));
+
+    return {
+        Highlight
+    };
+});
