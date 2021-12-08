079df4b7d917c36cd0b121fbef55324d53a8e67f
Ravi Lamkoti <ravilamkoti@gmail.com>
2021-12-08 11:11:47 +0530

    add: highlight

--- highlight/src/font-color/color.ts
+++
@@ -1,8 +0,0 @@
-const COLORS = [
-    { name: 'Default',  color: 'rgb(55, 53, 47)'},
-    { name: 'Gray',     color: 'rgba(55, 53, 47, 0.6)'},
-    { name: 'Brown',    color: 'rgb(100, 71, 58)'},
-    { name: 'Purple',   color: 'rgb(105, 64, 165)'}
-]
-
-export default COLORS
