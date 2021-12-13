9b32d5171197e3d00aca90ade71db76f8ed28319
Ravi Lamkoti <ravilamkoti@gmail.com>
2021-12-13 11:23:21 +0530

    fix: highlight

--- highlight/src/plugin.tsx
+++ highlight/src/plugin.tsx
@@ -10,8 +10,8 @@ export default ContentstackSDK.init().then(async (sdk) => {
 
     const Highlight = RTE('highlight', () => ({
         title: 'Highlight',
-        iconName: <Icon style={{padding: '0 6px'}} icon="Edit" size="original" />,
-        Component: (props) => {
+        icon: <Icon style={{padding: '0 6px'}} icon="Edit" size="original" />,
+        render: (props:any) => {
             return <span style={{background: 'rgba(251, 243, 219, 1)'}}>{props.children}</span>
         },
         display: ['toolbar'],
